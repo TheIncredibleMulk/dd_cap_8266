242ea6bc20a6cfe24e6076f5140340109b426176
TheIncredibleMulk <andrew.mulkey@gmail.com>
2021-01-16 23:28:14 -0500

    added research links

--- src/main.cpp
+++ src/main.cpp
@@ -3,22 +3,27 @@
 /*
  * CapitiveSense Library Demo Sketch
  * Paul Badger 2008
- * Uses a high value resistor e.g. 10M between send pin and receive pin
- * Resistor effects sensitivity, experiment with values, 50K - 50M. Larger resistor values yield larger sensor values.
+ * Modified for Project by Andrew Mulkey 2021
+ * Uses a high value resistor e.g. 470k between send pin and receive pin
+ * Resistor effects sensitivity, experiment with values, 50K - 1M. Larger resistor values yield larger sensor values.
  * Receive pin is the sensor pin - try different amounts of foil/metal on this pin
  */
 
+/* Adding values here so I can change the send and recieve pins easily
+var int send = 5
+var int recieve = 4
+*/
 
-CapacitiveSensor   cs_5_4 = CapacitiveSensor(5,4);        // 10M resistor between pins 4 & 8, pin 8 is sensor pin, add a wire and or foil
+CapacitiveSensor   cs_5_4 = CapacitiveSensor(5,4);        // 470k resistor between pins D2(5) & D3(4)
 
 void setup()                    
 {
-   //cs_5_4.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
+   cs_5_4.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
    cs_5_4.set_CS_Timeout_Millis(100000);
 
    Serial.begin(9600);
-   pinMode(LED_BUILTIN, OUTPUT);                         //set d13 as led output for sensing
-   pinMode(14, OUTPUT);
+   pinMode(LED_BUILTIN, OUTPUT);                         // set d as led output for sensing also uses the ESP's built in light to indicate sensing. 
+   pinMode(14, OUTPUT);                                  // set d5 as output for relay control
    }
 
 void loop()                    
